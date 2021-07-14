4db836d840345e915a59bdbf0ceb18989e46d763
AgusGelos <juanagustingelos1@gmail.com>
2021-07-14 10:48:20 -0300

    db conencted

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserModule } from './user/user.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,4 +1,7 @@
-import { Entity } from "typeorm";
+import { Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
-export class User {}
+export class User {
+	@PrimaryGeneratedColumn()
+	id: number;
+}

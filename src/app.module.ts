cfa41b57b975d94ad1e8b831ce21a3dbc2df862f
AgusGelos <juanagustingelos1@gmail.com>
2021-07-20 10:20:19 -0300

    fix(appointment)@c/OxROSk1e: Add repository pattern

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from './user/user.module';
+import { AppointmentModule } from './appointment/appointment.module';
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       entities: [__dirname + '/entities/**/*.entity{.ts,.js}'],
     }),
     UserModule,
+    AppointmentModule,
   ],
   controllers: [AppController],
   providers: [AppService],
